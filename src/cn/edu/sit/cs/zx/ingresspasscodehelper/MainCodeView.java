e43fcf8914abd7d02556d8f00bf7bff0b9559da4
hbzhuxu <hbzhuxu@yahoo.com.cn>
2013-06-22 22:14:27 +0800

    dure to the requirement of background Notify, create the branch to support this feature.

--- src/cn/edu/sit/cs/zx/ingresspasscodehelper/MainCodeView.java
+++ src/cn/edu/sit/cs/zx/ingresspasscodehelper/MainCodeView.java
@@ -2,6 +2,7 @@ package cn.edu.sit.cs.zx.ingresspasscodehelper;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -9,22 +10,21 @@ import java.util.List;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import cn.edu.sit.cs.zx.ingresspasscodehelper.util.SystemUiHider;
-
-import android.R.anim;
-import android.R.bool;
-import android.R.integer;
-import android.R.string;
+import android.R.layout;
+import android.R.id;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -32,28 +32,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Debug;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.TextureView;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import cn.edu.sit.cs.zx.ingresspasscodehelper.util.SystemUiHider;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -92,22 +89,54 @@ public class MainCodeView extends Activity {
 	private SystemUiHider mSystemUiHider;
 
 	private ListView mCodeList;
-	private List<String> mCodesHistory;
 	private ArrayList<HashMap<String, String>> mCodesMap;
 	private ClipboardManager clipboard = null;
+    private Calendar c;  
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main_code_view);
+		initSysUi();
 
+		//系统的初始化
+		mCodeList = (ListView) findViewById(R.id.listView1);
+		mCodesMap = new ArrayList<HashMap<String, String>>();
+		// Log.d(TAG,"-----------------" );
+		clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+		mCodeList.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View arg1,
+					int arg2, long arg3) {
+				TextView codeView = (TextView)arg1.findViewById(android.R.id.text1);
+				String tmpCodeString = codeView.getText().toString();
+				Log.d(TAG,""+String.valueOf(mCodesMap.get(arg2).get("item")));
+				SaveToClipboard(tmpCodeString);
+				try {
+					PackageManager packageManager = MainCodeView.this
+							.getPackageManager();
+					Intent intent = new Intent();
+					intent = packageManager
+							.getLaunchIntentForPackage("com.nianticproject.ingress");
+					getApplicationContext().startActivity(intent);
+				} catch (Exception e) {
+					Log.i(TAG, e.toString());
+				}
+				return false;
+			}
+
+		});
+
+		startService(new Intent("cn.edu.sit.cs.zx.ingresspasscodehelper.PasscodeFetchService"));
+	}
+
+	private void initSysUi() {
+		//原有系统自带的初始化
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
-
-		mCodeList = (ListView) findViewById(R.id.listView1);
-		mCodesHistory = new ArrayList<String>();
-		mCodesMap = new ArrayList<HashMap<String, String>>();
 
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
@@ -171,37 +200,12 @@ public class MainCodeView extends Activity {
 		// while interacting with the UI.
 		findViewById(R.id.dummy_button).setOnTouchListener(
 				mDelayHideTouchListener);
-
-		// Log.d(TAG,"-----------------" );
-		clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-
-		mCodeList.setOnItemLongClickListener(new OnItemLongClickListener() {
-
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View arg1,
-					int arg2, long arg3) {
-				TextView codeView = (TextView)arg1.findViewById(android.R.id.text1);
-				String tmpCodeString = codeView.getText().toString();
-				Log.d(TAG,""+String.valueOf(mCodesMap.get(arg2).get("item")));
-				SaveToClipboard(tmpCodeString);
-				try {
-					PackageManager packageManager = MainCodeView.this
-							.getPackageManager();
-					Intent intent = new Intent();
-					intent = packageManager
-							.getLaunchIntentForPackage("com.nianticproject.ingress");
-					getApplicationContext().startActivity(intent);
-				} catch (Exception e) {
-					Log.i(TAG, e.toString());
-				}
-				return false;
-			}
-
-		});
-
-		CodeHistoryLoad();
 	}
 
+    /**
+     * 保存字符串到剪贴板，并给出提示
+     * @param tmpCodeString 提示内容
+     */
 	private void SaveToClipboard(String tmpCodeString) {
 		Log.d(TAG, tmpCodeString);
 		ClipData textCd = ClipData.newPlainText("kkk", tmpCodeString);
@@ -209,46 +213,6 @@ public class MainCodeView extends Activity {
 		Toast.makeText(getApplicationContext(), "拷贝到剪贴板 " + tmpCodeString,
 				Toast.LENGTH_LONG).show();
 	}
-	
-	private void NotifyCode(String code) {
-		CodeHistorySave();
-
-	}
-	
-	
-	private Boolean CodeHistryCheck(String code)
-	{
-		for(String s : mCodesHistory){
-		      if(s.equals(code))
-		      return true;
-		}
-		return false;
-	}
-	
-	private void CodeHistorySave() {
-		StringBuilder csvList = new StringBuilder();
-		for(String s : mCodesHistory){
-		      csvList.append(s);
-		      csvList.append(",");
-		}
-		SharedPreferences preferences= getSharedPreferences("codeHistory", MODE_PRIVATE);
-		Editor sharedPreferencesEditor = preferences.edit();
-		sharedPreferencesEditor.putString("myList", csvList.toString());
-		sharedPreferencesEditor.commit();
-	}
-	
-	private void CodeHistoryLoad() {
-		SharedPreferences sharedPreferences= getSharedPreferences("codeHistory", MODE_PRIVATE);
-		String csvList = sharedPreferences.getString("myList","");
-		String[] items = csvList.split(",");
-		if(items.length<=0)
-			return;
-		mCodesHistory.clear();
-		for(int i=0; i < items.length; i++){
-			mCodesHistory.add(items[i]);     
-		}
-	}
-	
 	
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
@@ -270,10 +234,43 @@ public class MainCodeView extends Activity {
 	}
 
 	@Override
+	protected void onDestroy() {
+		// TODO 是否自动停止，或者加配置进行控制
+		//stopService(new Intent("cn.edu.sit.cs.zx.ingresspasscodehelper.PasscodeFetchService"));
+		super.onDestroy();
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.activity_main_code_menu, menu);
+		MenuItem item = menu.findItem(R.id.activity_main_code_menu_background_notify1);
+		if(item!=null)
+		{
+			boolean isStart = getSharedPreferences("PasscodeConfig",MODE_PRIVATE).getBoolean("isServiceRun", true);
+			item.setChecked(isStart);
+		}
+
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId()==R.id.activity_main_code_menu_background_notify1)
+		{
+			if(item.isChecked())
+			{
+				item.setChecked(false);
+				stopService(new Intent("cn.edu.sit.cs.zx.ingresspasscodehelper.PasscodeFetchService"));
+			}
+			else
+			{
+				item.setChecked(true);
+				startService(new Intent("cn.edu.sit.cs.zx.ingresspasscodehelper.PasscodeFetchService"));
+			}
+			getSharedPreferences("PasscodeConfig",MODE_PRIVATE).edit().putBoolean("isServiceRun", item.isChecked()).commit();
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	/**
@@ -301,7 +298,10 @@ public class MainCodeView extends Activity {
 				
 				Long long1 = Long.parseLong(iteMap.get("time"));
 				int m = (int) ((date.getTime()/1000 - long1)/60)+1;
-				iteMap.put("desc", String.valueOf(m)+"分钟前 ["+iteMap.get("point")+"] "+iteMap.get("item"));
+				String timeDesc = String.valueOf(m % 60)+"分钟前                        ";
+				if((m-m % 60)/60>0)
+					timeDesc=String.valueOf((m-m % 60)/60)+"小时"+timeDesc;
+				iteMap.put("desc", timeDesc+" [Coxxs指数："+iteMap.get("point")+"]                "+iteMap.get("item"));
 	         } 
 
 			// 操作界面
@@ -333,18 +333,11 @@ public class MainCodeView extends Activity {
 			HttpGet httpGet = new HttpGet(url);
 			// 第二步，使用execute方法发送HTTP GET请求，并返回HttpResponse对象
 			HttpResponse httpResponse;
+			//AccountManager manager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
+			//Account[] list = manager.getAccounts();
 			try {
 				HttpClient httpclient = new DefaultHttpClient();
-				httpGet.setHeader(
-						"User-Agent",
-						"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.6) Gecko/20100625Firefox/3.6.6 Greatwqs");
-				httpGet.setHeader("Accept",
-						"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-				httpGet.setHeader("Accept-Language", "zh-cn,zh;q=0.5");
-				httpGet.setHeader("Host", "gressbot.com");
-				httpGet.setHeader("Accept-Charset",
-						"ISO-8859-1,utf-8;q=0.7,*;q=0.7");
-				httpGet.setHeader("Referer", "http://gressbot.com");
+				initHttpHeader(httpGet);
 
 				httpResponse = httpclient.execute(httpGet);
 				// Log.d(TAG,EntityUtils.toString(httpResponse.getEntity()));
@@ -355,25 +348,7 @@ public class MainCodeView extends Activity {
 							.getEntity());
 					Log.d(TAG, result);
 
-					try {
-						JSONObject param = new JSONObject(result);
-						JSONArray codeArray = param.getJSONArray("result");
-						//mCodes = new String[codeArray.length()];
-						mCodesMap.clear();
-
-						for (int i = 0; i < codeArray.length(); i++) {
-							JSONObject object = codeArray.getJSONObject(i);
-							HashMap<String, String> iteMap = new HashMap<String, String>();
-							iteMap.put("code", object.getString("code"));
-							iteMap.put("item", object.getString("item"));
-							iteMap.put("time", object.getString("time"));
-							iteMap.put("point", object.getString("point"));
-							mCodesMap.add(iteMap);
-						}
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					processHttpResult(result);
 				}
 			} catch (ClientProtocolException e) {
 				// TODO Auto-generated catch block
@@ -382,9 +357,44 @@ public class MainCodeView extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
+			//提示UI线程绑定数据
 			mHideHandler.sendEmptyMessage(1);
 
+		}
+
+		private void processHttpResult(String result) {
+			try {
+				JSONObject param = new JSONObject(result);
+				JSONArray codeArray = param.getJSONArray("result");
+				//mCodes = new String[codeArray.length()];
+				mCodesMap.clear();
+
+				for (int i = 0; i < codeArray.length(); i++) {
+					JSONObject object = codeArray.getJSONObject(i);
+					HashMap<String, String> iteMap = new HashMap<String, String>();
+					iteMap.put("code", object.getString("code"));
+					iteMap.put("item", object.getString("item"));
+					iteMap.put("time", object.getString("time"));
+					iteMap.put("point", object.getString("point"));
+					mCodesMap.add(iteMap);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		private void initHttpHeader(HttpGet httpGet) {
+			httpGet.setHeader(
+					"User-Agent",
+					"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.6) Gecko/20100625Firefox/3.6.6 Greatwqs");
+			httpGet.setHeader("Accept",
+					"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+			httpGet.setHeader("Accept-Language", "zh-cn,zh;q=0.5");
+			httpGet.setHeader("Host", "gressbot.com");
+			httpGet.setHeader("Accept-Charset",
+					"ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+			httpGet.setHeader("Referer", "http://gressbot.com");
 		}
 	};
 
@@ -396,4 +406,31 @@ public class MainCodeView extends Activity {
 		// mHideHandler.removeCallbacks(mHideRunnable);
 		// mHideHandler.postDelayed(mHideRunnable, delayMillis);
 	}
+	
+   
+    /**
+     * 用来判断服务是否运行.
+     * @param context
+     * @param className 判断的服务名字
+     * @return true 在运行 false 不在运行
+     */
+    public static boolean isServiceRunning(Context mContext,String className) {
+        boolean isRunning = false;
+		ActivityManager activityManager = (ActivityManager)
+		mContext.getSystemService(Context.ACTIVITY_SERVICE); 
+        List<ActivityManager.RunningServiceInfo> serviceList 
+        = activityManager.getRunningServices(30);
+       if (!(serviceList.size()>0)) {
+            return false;
+        }
+        for (int i=0; i<serviceList.size(); i++) {
+            if (serviceList.get(i).service.getClassName().equals(className) == true) {
+                isRunning = true;
+                break;
+            }
+        }
+        return isRunning;
+    }
+      
+	
 }
--- src/cn/edu/sit/cs/zx/ingresspasscodehelper/PasscodeFetchService.java
+++ src/cn/edu/sit/cs/zx/ingresspasscodehelper/PasscodeFetchService.java
@@ -1,29 +1,265 @@
 package cn.edu.sit.cs.zx.ingresspasscodehelper;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.R.integer;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.util.Log;
 
 public class PasscodeFetchService extends Service {
-    // Binder given to clients
-    private final IBinder mBinder = new LocalBinder();
+	static String TAG = "PasscodeFetchService";
+	private static int MOOD_NOTIFICATIONS = 1;
+	private Notification mN;
+	private NotificationManager mNm;
+	private Intent mI;
+	private PendingIntent mP;
+	private List<String> mCodesHistory;
+	private Boolean mIsFetch = false;
+	private int mSleepCount;
 
-    /**
-     * Class used for the client Binder.  Because we know this service always
-     * runs in the same process as its clients, we don't need to deal with IPC.
-     */
-    public class LocalBinder extends Binder {
-    	PasscodeFetchService getService() {
-            // Return this instance of LocalService so clients can call public methods
-            return PasscodeFetchService.this;
-        }
-    }
+	// Binder given to clients
+	private final IBinder mBinder = new LocalBinder() {
+		@Override
+		protected boolean onTransact(int code, Parcel data, Parcel reply,
+				int flags) throws RemoteException {
+			return super.onTransact(code, data, reply, flags);
+		}
+	};
+
+	/**
+	 * Class used for the client Binder. Because we know this service always
+	 * runs in the same process as its clients, we don't need to deal with IPC.
+	 */
+	public class LocalBinder extends Binder {
+		PasscodeFetchService getService() {
+			// Return this instance of LocalService so clients can call public
+			// methods
+			return PasscodeFetchService.this;
+		}
+	}
 
 	@Override
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
 		return mBinder;
+	}
+
+	@Override
+	public void onDestroy() {
+		mNm.cancel(MOOD_NOTIFICATIONS);
+		mIsFetch = false;
+		super.onDestroy();
+	}
+
+	@Override
+	public void onCreate() {
+		// 预防重复通知的结构
+		mCodesHistory = new ArrayList<String>();
+		CodeHistoryLoad();
+
+		// 初始化通知
+		initNotify();
+		// 开启新的线程从服务器获取数据
+		Thread getArticlesThread = new Thread(null, mTask, "getNewPasscodes");
+		getArticlesThread.start();
+
+		super.onCreate();
+	}
+
+	private void initNotify() {
+		mN = new Notification();
+		// img
+		mN.icon = R.drawable.ic_launcher;
+		// text
+		mN.tickerText = getString(R.string.service_passcode_fetch_notify_title);
+		// audio
+		mN.defaults = Notification.DEFAULT_SOUND;
+		// click and auto_cancel
+		mN.flags = Notification.FLAG_AUTO_CANCEL;
+		mNm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		// 点击顶部状态栏的消息后，welcomeactivity为需要跳转的页面
+		mI = new Intent(this, MainCodeView.class);
+		mP = PendingIntent.getActivity(this, 0, mI, 0);
+	}
+
+	Runnable mTask = new Runnable() {
+
+		@Override
+		public void run() {
+			Log.d(TAG, "start connect webpage");
+			mIsFetch = true;
+			while (mIsFetch) {
+				if(mSleepCount % 12==0)
+				{
+					fetchPasscode();
+					mSleepCount=0;
+				}
+				try {
+					Thread.sleep(5000);
+					mSleepCount++;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		private void fetchPasscode() {
+			
+			ConnectivityManager conMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo networkInfo= conMan.getActiveNetworkInfo();
+			if(networkInfo== null || !networkInfo.isConnected())
+				return;
+			
+			String url = "http://gressbot.com/?mod=passcodeapi";
+			// 第一步，创建HttpGet对象
+			HttpGet httpGet = new HttpGet(url);
+			// 第二步，使用execute方法发送HTTP GET请求，并返回HttpResponse对象
+			HttpResponse httpResponse;
+			
+			Log.d(TAG,"start fetch");
+			// AccountManager manager = (AccountManager)
+			// getSystemService(ACCOUNT_SERVICE);
+			// Account[] list = manager.getAccounts();
+			try {
+				HttpClient httpclient = new DefaultHttpClient();
+				initHttpHeader(httpGet);
+
+				httpResponse = httpclient.execute(httpGet);
+				// Log.d(TAG,EntityUtils.toString(httpResponse.getEntity()));
+
+				if (httpResponse.getStatusLine().getStatusCode() == 200) {
+					// 第三步，使用getEntity方法活得返回结果
+					String result = EntityUtils.toString(httpResponse
+							.getEntity());
+					Log.d(TAG, result);
+
+					processHttpResult(result);
+				}
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		private void processHttpResult(String result) {
+			try {
+				JSONObject param = new JSONObject(result);
+				JSONArray codeArray = param.getJSONArray("result");
+				Log.d(TAG,"finish fetch. total:"+String.valueOf(codeArray.length()));
+				// TODO:此处可能会有逻辑上的异常，比如老code重复发notify，需要仔细考虑
+				if (codeArray.length() == 0) {
+					mCodesHistory.clear();
+					CodeHistorySave();
+					return;
+				}
+				// 处理code
+				int codeCount = 0;
+				for (int i = 0; i < codeArray.length(); i++) {
+					JSONObject object = codeArray.getJSONObject(i);
+					// 检查是否有新code
+					if (CodeHistryCheck(object.getString("code")))
+						continue;
+					mCodesHistory.add(object.getString("code"));
+					codeCount++;
+				}
+				if (codeCount > 0) {
+					CodeHistorySave();
+					showNotification("New Code Comming +"
+							+ String.valueOf(codeCount));
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		private void initHttpHeader(HttpGet httpGet) {
+			httpGet.setHeader(
+					"User-Agent",
+					"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.6) Gecko/20100625Firefox/3.6.6 Greatwqs");
+			httpGet.setHeader("Accept",
+					"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+			httpGet.setHeader("Accept-Language", "zh-cn,zh;q=0.5");
+			httpGet.setHeader("Host", "gressbot.com");
+			httpGet.setHeader("Accept-Charset",
+					"ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+			httpGet.setHeader("Referer", "http://gressbot.com");
+		}
+	};
+
+	private void showNotification(String contentText) {
+//		mN =new Notification.Builder(PasscodeFetchService.this)
+//        .setContentTitle(getString(R.string.service_passcode_fetch_notify_title))
+//        .setContentText(contentText)
+//        .setSmallIcon(R.drawable.ic_launcher)
+//        .build();
+		mN.setLatestEventInfo(PasscodeFetchService.this,
+				getString(R.string.service_passcode_fetch_notify_title),
+				contentText, mP);
+		mNm.notify(MOOD_NOTIFICATIONS, mN);
+	}
+
+	private Boolean CodeHistryCheck(String code) {
+		for (String s : mCodesHistory) {
+			if (s.equals(code))
+				return true;
+		}
+		return false;
+	}
+
+	private void CodeHistorySave() {
+		StringBuilder csvList = new StringBuilder();
+		for (String s : mCodesHistory) {
+			csvList.append(s);
+			csvList.append(",");
+		}
+		SharedPreferences preferences = getSharedPreferences("codeHistory",
+				MODE_PRIVATE);
+		Editor sharedPreferencesEditor = preferences.edit();
+		sharedPreferencesEditor.putString("myList", csvList.toString());
+		sharedPreferencesEditor.commit();
+	}
+
+	private void CodeHistoryLoad() {
+		SharedPreferences sharedPreferences = getSharedPreferences(
+				"codeHistory", MODE_PRIVATE);
+		String csvList = sharedPreferences.getString("myList", "");
+		String[] items = csvList.split(",");
+		if (items.length <= 0)
+			return;
+		mCodesHistory.clear();
+		for (int i = 0; i < items.length; i++) {
+			mCodesHistory.add(items[i]);
+		}
 	}
 
 }
